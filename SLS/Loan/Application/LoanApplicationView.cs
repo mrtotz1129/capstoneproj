617f80b1658bb8caab78dcc77b640ddde9c90c8a
Emmanuel Alcartado <ejalcartado@gmail.com>
2015-10-02 15:39:00 +0800

    Save Function for Loan Application

---
+++ SLS/Loan/Application/LoanApplicationView.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace SLS.Loan.Application
+{
+
+    public partial class LoanApplicationView : Form
+    {
+        public LoanApplicationView()
+        {
+            InitializeComponent();
+            this.lOANTableAdapter.FillBy(this.sLSDBDataSet1.LOAN);
+        }
+
+        private void LoanApplicationView_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void btnLoan_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+    }
+}
