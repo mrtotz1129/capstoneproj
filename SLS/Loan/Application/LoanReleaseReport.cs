a391f5a71b282806a213d20a15bd31387f0dd682
Kinit <kenavecilla@gmail.com>
2015-10-02 08:07:24 +0800

    Reports with form

---
+++ SLS/Loan/Application/LoanReleaseReport.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+
+namespace SLS.Loan.Application
+{
+    public partial class LoanReleaseReport : Form
+    {
+        public LoanReleaseReport()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            ReportDocument cryRpt = new ReportDocument();
+            cryRpt.Load("D:\\capstoneproj\\SLS\\SummaryOfReleasedLoans.rpt");
+
+            ParameterFieldDefinitions crParameterFieldDefinitions;
+            ParameterFieldDefinition crParameterFieldDefinition;
+            ParameterValues crParameterValues = new ParameterValues();
+            ParameterDiscreteValue crParameterDiscreteValue = new ParameterDiscreteValue();
+
+            crParameterDiscreteValue.Value = dtFrom.Text;
+            crParameterFieldDefinitions = cryRpt.DataDefinition.ParameterFields;
+            crParameterFieldDefinition = crParameterFieldDefinitions["DateFrom"];
+            crParameterValues = crParameterFieldDefinition.CurrentValues;
+
+            crParameterValues.Clear();
+            crParameterValues.Add(crParameterDiscreteValue);
+            crParameterFieldDefinition.ApplyCurrentValues(crParameterValues);
+
+            crParameterDiscreteValue.Value = dtTo.Text;
+            crParameterFieldDefinitions = cryRpt.DataDefinition.ParameterFields;
+            crParameterFieldDefinition = crParameterFieldDefinitions["DateTo"];
+            crParameterValues = crParameterFieldDefinition.CurrentValues;
+
+            crParameterValues.Add(crParameterDiscreteValue);
+            crParameterFieldDefinition.ApplyCurrentValues(crParameterValues);
+
+            crystalReportViewer1.ReportSource = cryRpt;
+            crystalReportViewer1.Refresh(); 
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
